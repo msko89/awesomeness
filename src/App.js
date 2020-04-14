aae8525dadbfce916357c93e471f9cc041d2f1bd
smartsfa09 <smartsfa09@lottechilsung.co.kr>
2020-04-14 16:41:31 +0900

    Delete file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import styled, { createGlobalStyle, css, keyframes } from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const GlobalStyle = createGlobalStyle`
+  body{
+    padding: 0;
+    margin: 0;
+  }
+`;
+
+const Input = styled.input.attrs({
+  required: true
+})`
+  border-radius: 5px;
+`;
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <GlobalStyle />
+        <Container>
+          <Input placeholder="hello" />
+        </Container>
+      </Fragment>
+    );
+  }
 }
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: pink;
+`;
 
 export default App;
